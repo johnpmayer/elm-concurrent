90ed0af98adb54a4e426b4c8bc4742412a09f1df
John P Mayer, Jr <john.p.mayer.jr@gmail.com>
2016-01-01 17:28:23 -0500

    untested task primitives

--- src/TaskUtils.elm
+++ src/TaskUtils.elm
@@ -3,6 +3,5 @@ module TaskUtils (andThen_) where
 
 import Task exposing (Task, andThen)
 
-andThen_ : Task x () -> Task x b -> Task x b
+andThen_ : Task x a -> Task x b -> Task x b
 andThen_ t1 t2 = t1 `andThen` (\_ -> t2)
-
