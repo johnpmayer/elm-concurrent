d60597d44a4738ee3d2c9c6899b5b3ee08ff6076
John P Mayer, Jr <john.p.mayer.jr@gmail.com>
2016-01-01 18:11:50 -0500

    moved Future into its own module

--- src/Concurrent/Task.elm
+++ src/Concurrent/Task.elm
@@ -1,66 +1,20 @@
 
 module Concurrent.Task
-  ( Future, future, wait
-  , onSuccess, onFailure
-  , waitBoth
+  ( waitBoth
   , map2
   ) where
 
 {-|
-
-@docs Future, future, wait
-
-@docs onSuccess, onFailure
 
 @docs waitBoth
 
 @docs map2
 
 -}
-  
+
 import Result exposing (Result)
-import Task exposing (Task, ThreadID, andThen, fail, fromResult, map, onError, spawn, succeed, toResult)
-import TaskUtils exposing (andThen_)
-import Concurrent.MVar exposing (MVar, newEmptyMVar, putMVar, readMVar, takeMVar)
-
-{-| A handle to a result that may be fulfilled -}
-type Future x a = Future (MVar (Result x a))
-
-{-| Convert a task into a future -}
-future : Task x a -> Task x (Future x a)
-future task =
-  newEmptyMVar `andThen` \var ->
-  spawn (toResult task `andThen` putMVar var) `andThen_`
-  succeed (Future var)
-
-{-| Wait for a future to resolve -}
-wait : Future x a -> Task x a
-wait (Future var) = 
-  readMVar var `andThen` fromResult
-
-{-| 
-Register a task to run if the future succeeds
-Nothing is done if the registered task itself fails
--}
-onSuccess : Future x a -> (a -> Task y b) -> Task z ThreadID
-onSuccess (Future var) task =
-  spawn <|
-    readMVar var `andThen` \result ->
-    case result of
-      Ok a -> task a `andThen_` succeed ()
-      Err e -> succeed ()
-
-{-| 
-Register a task to run if the future fails
-Nothing is done if the registered task itself fails
--}
-onFailure : Future x a -> (x -> Task y b) -> Task z ThreadID
-onFailure (Future var) task =
-  spawn <|
-    readMVar var `andThen` \result ->
-    case result of
-      Ok a -> succeed ()
-      Err e -> task e `andThen_` succeed ()
+import Task exposing (Task, andThen, map, succeed)
+import Concurrent.Future exposing (Future, future, wait)
 
 {-| 
 Run both tasks in parallel, returning both results
