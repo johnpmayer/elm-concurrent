75d79a1f3a4c2fd8a247c0f3cf1990ceb7a88d38
John P Mayer, Jr <john.p.mayer.jr@gmail.com>
2016-01-01 16:50:16 -0500

    fix the fill drain test

--- examples/FillDrain.elm
+++ examples/FillDrain.elm
@@ -3,35 +3,40 @@ module FillDrain where
 
 import Concurrent.Chan exposing (Chan, newChan, writeChan, readChan)
 import Debug exposing (log)
-import Graphics.Element exposing (Element, show)
-import Signal exposing (constant, Mailbox, mailbox, map, send)
+import Graphics.Element exposing (Element, down, flow, show)
+import Signal exposing (constant, Mailbox, mailbox, map, map2, send)
 import Task exposing (Task, andThen, sleep, spawn, succeed)
 
-drainResult : Mailbox (Maybe Int)
+fillResult : Mailbox (Maybe String)
+fillResult = mailbox Nothing
+
+drainResult : Mailbox (Maybe String)
 drainResult = mailbox Nothing
 
 main : Signal Element
-main = map show drainResult.signal
+main = map (\(fill, drain) -> flow down [show fill, show drain]) (map2 (,) fillResult.signal drainResult.signal)
 
-producer : Int -> Chan Int -> Task x ()
-producer x chan = 
-  writeChan chan x  `andThen` \_ ->
-  sleep 100         `andThen` \_ ->
-    if x > 0
-    then producer (x + 1) chan
+produceN : Int -> Int -> Chan Int -> Task x ()
+produceN x max chan = 
+  send fillResult.address (Just ("Fill " ++ toString x)) `andThen` \_ ->
+  writeChan chan x                                      `andThen` \_ ->
+  sleep 100                                             `andThen` \_ ->
+    if x < max
+    then produceN (x + 1) max chan
     else succeed ()
 
 consumer : Chan Int -> Task x ()
 consumer chan = 
-  readChan chan                     `andThen` \x ->
-  send drainResult.address (Just x) `andThen` \_ ->
+  readChan chan                                           `andThen` \x ->
+  send drainResult.address (Just ("Drain " ++ toString x)) `andThen` \_ ->
   consumer chan
 
--- Demonstrate that the chan can fill up
+-- Demonstrate that the chan can fill up without blocking, and then drain & keep filling concurrently
 
 port startup : Task x ()
 port startup = 
-  newChan               `andThen` \chan ->
-  producer 10 chan      `andThen` \_ ->
-  spawn (consumer chan) `andThen` \_ ->
+  newChan                     `andThen` \chan ->
+  produceN 0 15 chan          `andThen` \_ ->
+  spawn (consumer chan)       `andThen` \_ ->
+  spawn (produceN 15 50 chan) `andThen` \_ ->
   succeed ()
