da10f286cbc294aa6715a914562fb73cf9eca836
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2016-01-04 19:06:50 -0500

    lose some 'safety'

--- src/Concurrent/Future.elm
+++ src/Concurrent/Future.elm
@@ -13,33 +13,34 @@ module Concurrent.Future
 -}
 
 import Result exposing (Result)
-import Task exposing (Task, ThreadID, andThen, fail, fromResult, map, onError, spawn, succeed, toResult)
-import TaskUtils exposing (andThen_)
+import Task exposing (Task, ThreadID, andThen, fail, fromResult, onError, spawn, succeed, toResult)
+import TaskUtils exposing (Never, andThen_, unsafeFromNever)
 import Concurrent.MVar exposing (MVar, newEmptyMVar, putMVar, readMVar, takeMVar)
 
 {-| A handle to a result that may be fulfilled -}
-type Future x a = Future (MVar (Result x a))
+type Future x a = Future { await : Task Never (Result x a) }
 
 {-| Convert a task into a future -}
 future : Task x a -> Task x (Future x a)
 future task =
   newEmptyMVar `andThen` \var ->
   spawn (toResult task `andThen` putMVar var) `andThen_`
-  succeed (Future var)
+  let await = readMVar var
+  in succeed (Future { await = await })
 
 {-| Wait for a future to resolve -}
 wait : Future x a -> Task x a
-wait (Future var) = 
-  readMVar var `andThen` fromResult
-
+wait (Future {await}) = 
+  unsafeFromNever await `andThen` fromResult
+    
 {-| 
 Register a task to run if the future succeeds
 Nothing is done if the registered task itself fails
 -}
 onSuccess : Future x a -> (a -> Task y b) -> Task z ThreadID
-onSuccess (Future var) task =
+onSuccess (Future {await}) task =
   spawn <|
-    readMVar var `andThen` \result ->
+    unsafeFromNever await `andThen` \result ->
     case result of
       Ok a -> task a `andThen_` succeed ()
       Err e -> succeed ()
@@ -49,9 +50,13 @@ Register a task to run if the future fails
 Nothing is done if the registered task itself fails
 -}
 onFailure : Future x a -> (x -> Task y b) -> Task z ThreadID
-onFailure (Future var) task =
+onFailure (Future {await}) task =
   spawn <|
-    readMVar var `andThen` \result ->
+    unsafeFromNever await `andThen` \result ->
     case result of
       Ok a -> succeed ()
       Err e -> task e `andThen_` succeed ()
+
+{-| 'Functor' interface for Future -}
+map : (a -> b) -> Future x a -> Future x b
+map fun (Future {await}) = Future { await = Task.map (Result.map fun) await }
--- src/TaskUtils.elm
+++ src/TaskUtils.elm
@@ -1,7 +1,13 @@
 
-module TaskUtils (andThen_) where
+module TaskUtils (Never, andThen_, unsafeFromNever) where
 
-import Task exposing (Task, andThen)
+import Task exposing (Task, andThen, fail, onError)
+import Native.TaskUtils exposing (fromNever)
 
 andThen_ : Task x a -> Task x b -> Task x b
 andThen_ t1 t2 = t1 `andThen` (\_ -> t2)
+
+type Never = Never_
+
+unsafeFromNever : Task Never a -> Task x a
+unsafeFromNever = Native.TaskUtils.fromNever
